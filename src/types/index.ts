82a276bf31f52a4b4898fdc2400547e6112faf0b
Simon-157 <boatengsimonjnr157@gmail.com>
2024-04-28 09:56:20 +0000

    feat: added judgeO logic to code editor

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+import { languageOptions } from '../lib/constants';
+export type Judge0SubmissionOutput = {
+  stdout?: string;
+  stderr?: string;
+  compile_output?: string;
+  message?: string;
+  exit_code?: number;
+  exit_signal?: number;
+  status?: {
+    id: number;
+    description: string;
+  };
+  created_at?: Date;
+  finished_at?: Date | null;
+  token?: string;
+  time?: number;
+  wall_time?: number;
+  memory_used?: number;
+}
+
+
+export type languageOptionsType = typeof languageOptions
