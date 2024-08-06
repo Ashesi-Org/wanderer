dce1b62672c759470d90f0b2bcb99b7ff5271564
urdad <70736338+urdadx@users.noreply.github.com>
2024-08-06 23:40:37 +0000

    ready for deployment number 5

--- src/app/(problems)/layout.tsx
+++ src/app/(problems)/layout.tsx
@@ -1,38 +1,6 @@
 import { ReactNode } from "react";
 import Navbar from "../ui/navbar";
-import { Suspense } from "react";
 import React from 'react';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-
-export const SkeletonTable = () => {
-    const skeletonRows = Array.from({ length: 5 }).map((_, index) => (
-        <TableRow key={index}>
-            <TableCell className="animate-pulse bg-gray-300 h-8 w-full"></TableCell>
-            <TableCell className="animate-pulse bg-gray-300 h-8 w-full"></TableCell>
-            <TableCell className="animate-pulse bg-gray-300 h-8 w-full"></TableCell>
-            <TableCell className="animate-pulse bg-gray-300 h-8 w-full"></TableCell>
-        </TableRow>
-    ));
-
-    return (
-        <div className="rounded-md border">
-            <Table>
-                <TableHeader className="bg-secondary">
-                    <TableRow>
-                        <TableHead className="animate-pulse bg-gray-300 h-8 w-full"></TableHead>
-                        <TableHead className="animate-pulse bg-gray-300 h-8 w-full"></TableHead>
-                        <TableHead className="animate-pulse bg-gray-300 h-8 w-full"></TableHead>
-                        <TableHead className="animate-pulse bg-gray-300 h-8 w-full"></TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {skeletonRows}
-                </TableBody>
-            </Table>
-        </div>
-    );
-};
-
 
 
 const TableLayout = ({ children }: { children: ReactNode }) => {
